0a7b16d7c97c320fb85d4d7e50a0ac694cd37c86
Bhavesh Pothula <bhaveshpothula@Bhaveshs-MacBook-Air.local>
2026-06-02 11:58:13 +0530

    inventory history page

---
+++ src/app/inventory-history/page.tsx
@@ -0,0 +1,9 @@
+export default async function InventoryHistoryPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">
+        Inventory History
+      </h1>
+    </div>
+  );
+}
